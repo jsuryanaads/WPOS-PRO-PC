cd537f64c68aa23c6ff641a9c84a6db61c8a6468
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-11 21:54:56 +0700

    Wire product and stock UX polish

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from .ui.ux2026 import apply_ux2026
 from .ui.unified_ui import apply_unified_ui
 from .ui.dashboard_ux import apply_dashboard_ux
 from .ui.purchase_ux import apply_purchase_ux
+from .ui.product_stock_ux import apply_product_stock_ux
 from .ui.themes import apply_theme, current_theme
 
 
@@ -30,6 +31,7 @@ def main():
         apply_unified_ui(window)
         apply_dashboard_ux(window)
         apply_purchase_ux(window)
+        apply_product_stock_ux(window)
 
     def success(user):
         def logout_callback(window):
